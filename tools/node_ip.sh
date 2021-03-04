f9f508a4c8dc9d976b95d3eb4738b9c51696c117
Nickolay Ihalainen <ihanick@gmail.com>
2021-03-04 09:54:21 +0300

    IMP: caching for os <> el7

--- tools/node_ip.sh
+++ tools/node_ip.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DEV=$(/sbin/ip ro ls | grep default | cut -d' ' -f 5)
+if [ "x$DEV" == "x" ] ; then
+  echo "127.0.0.1"
+  exit 1
+fi
 # virtualbox with single ip nat has default ip 10.0.2.15
 if /sbin/ip -o -4 addr list eth0 2>/dev/null|grep -F 10.0.2.15/24 &>/dev/null ; then DEV=eth1 ; fi
 ip4=$(/sbin/ip -o -4 addr list $DEV | awk '{print $4}' | cut -d/ -f1)
