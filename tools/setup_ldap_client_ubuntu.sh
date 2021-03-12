d0b442088c7007ebb52992076196b46e99bd5ede
Nickolay Ihalainen <ihanick@gmail.com>
2021-03-12 18:09:42 +0300

    IMP: Debian 9 (stretch) and Debian 10 (buster) containers with os:stretch and os:buster, FIX: PG HAProxy uses xinetd checks, FIX: do not other packages in user_security role, IMP: OpenLDAP client on buster

--- tools/setup_ldap_client_ubuntu.sh
+++ tools/setup_ldap_client_ubuntu.sh
@@ -17,6 +17,18 @@ ldap-auth-config ldap-auth-config/ldapns/base-dn string dc=percona,dc=local
 ldap-auth-config ldap-auth-config/pam_password select md5
 ldap-auth-config ldap-auth-config/dbrootlogin boolean true
 ldap-auth-config ldap-auth-config/binddn string cn=ldapadm,dc=percona,dc=local
+libnss-ldap:amd64 libnss-ldap/rootbindpw password $LDAP_PASS
+libnss-ldap:amd64 libnss-ldap/bindpw password $LDAP_PASS
+libnss-ldap:amd64 libnss-ldap/rootbinddn string cn=ldapadm,dc=percona,dc=local
+libnss-ldap:amd64 libnss-ldap/binddn string cn=ldapadm,dc=percona,dc=local
+libnss-ldap:amd64 libnss-ldap/nsswitch note
+libnss-ldap:amd64 shared/ldapns/ldap_version select 3
+libnss-ldap:amd64 libnss-ldap/dblogin boolean false
+libnss-ldap:amd64 libnss-ldap/override boolean true
+libnss-ldap:amd64 libnss-ldap/confperm boolean false
+libnss-ldap:amd64 shared/ldapns/ldap-server string ldap://$LDAP_IP/
+libnss-ldap:amd64 shared/ldapns/base-dn string dc=percona,dc=local
+libnss-ldap:amd64 libnss-ldap/dbrootlogin boolean true
 EOF
 
 DEBIAN_FRONTEND=noninteractive apt-get -y install libnss-ldap libpam-ldap ldap-utils
@@ -27,7 +39,9 @@ EOF
 cat >> /etc/pam.d/common-session <<EOF
 session optional pam_mkhomedir.so skel=/etc/skel umask=077
 EOF
-sed -i -e 's/files systemd$/files systemd ldap/' /etc/nsswitch.conf
+sed -i -re 's/^(.*enable-cache.*(group|passwd).*)yes/\1no/' /etc/nscd.conf
+systemctl restart nscd || true
+grep -q ldap /etc/nsswitch.conf || sed -i -re 's/(^(passwd|group):.*)$/\1 ldap/' /etc/nsswitch.conf
 # ldapsearch -x cn=dba -b dc=percona,dc=local
 # getent passwd dba
 touch /root/ldap-client.configured
