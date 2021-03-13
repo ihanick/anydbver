eac463834f555905aecd6fb4accd6e440c8fee1f
Nickolay Ihalainen <ihanick@gmail.com>
2021-03-13 18:15:45 +0300

    IMP: mongos on focal, FIX: mongos on el7 and el8 remove hardcoded cfg0-keyfile value, IMP: ./anydbver deploy --help

--- tools/setup_mongos.sh
+++ tools/setup_mongos.sh
@@ -4,40 +4,6 @@ USR=$1 # dba
 PASS=$2 # secret
 SHARDSRV=$3 # rs0/10.76.129.55:27017,10.76.129.229:27017,10.76.129.3:27017
 
-
-cat > /etc/sysconfig/mongos <<EOF
-OPTIONS="-f /etc/mongos.conf"
-STDOUT="/var/log/mongo/mongos.stdout"
-STDERR="/var/log/mongo/mongos.stderr"
-NUMACTL="numactl --interleave=all"
-EOF
-
-cat > /etc/systemd/system/mongos.service <<EOF
-[Unit]
-Description=High-performance, schema-free document-oriented database
-After=time-sync.target network.target
-
-[Service]
-Type=forking
-User=mongod
-Group=mongod
-PermissionsStartOnly=true
-LimitFSIZE=infinity
-LimitCPU=infinity
-LimitAS=infinity
-LimitNOFILE=64000
-LimitNPROC=64000
-EnvironmentFile=-/etc/sysconfig/mongos
-ExecStartPre=/usr/bin/percona-server-mongodb-helper.sh
-ExecStart=/usr/bin/env bash -c "\${NUMACTL} /usr/bin/mongos \${OPTIONS} > \${STDOUT} 2> \${STDERR}"
-PIDFile=/var/run/mongod.pid
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl daemon-reload
-
 KEYFILE=$(yq r /etc/mongos.conf security.keyFile)
 KEYFILE_NAME=$(basename "$KEYFILE")
 cp "/vagrant/secret/$KEYFILE_NAME" "$KEYFILE"
@@ -58,3 +24,5 @@ for SHARD_ITEM in $FULL_NEW_SEP ; do
 done
 
 IFS=OLDIFS
+
+touch /root/mongos.configured
