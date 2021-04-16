dd7cfe4d45b35a3a6099e0f778a28d28a4c517c6
Nickolay Ihalainen <ihanick@gmail.com>
2021-04-16 16:57:27 +0300

    IMP: Postgresql logical replication

--- tools/setup_postgresql_replication.sh
+++ tools/setup_postgresql_replication.sh
@@ -6,6 +6,7 @@ MASTER_PASSWORD=$4
 MASTER_DB=$5
 PGDATA=$6
 SYSTEMD_UNIT=$7
+LOGICAL_DB=$8
 
 SERVER_ID=$(ip addr ls|grep 'inet '|grep -v '127.0.0.1'|awk '{print $2}'|cut -d/ -f 1|awk -F '\\.' '{print ($1 * 2^24) + ($2 * 2^16) + ($3 * 2^8) + $4}')
 
@@ -46,4 +47,21 @@ EOF
     # it's a next step in playbook: systemctl start $SYSTEMD_UNIT
 
     touch /root/replication.configured
+elif [[ "x$TYPE" == "xlogical" ]] ; then
+    unset PGHOSTADDR
+    PGPASSWORD="$MASTER_PASSWORD" PGHOST=$(node_ip.sh) PGDATABASE=$MASTER_DB PGUSER=$MASTER_USER psql <<EOF
+CREATE USER sbtest WITH PASSWORD '$MASTER_PASSWORD';
+CREATE DATABASE $LOGICAL_DB;
+GRANT ALL PRIVILEGES ON DATABASE $LOGICAL_DB TO sbtest;
+EOF
+    PGPASSWORD="$MASTER_PASSWORD" PGHOST=$MASTER_IP PGDATABASE=$LOGICAL_DB PGUSER=$MASTER_USER psql <<EOF
+CREATE PUBLICATION ${LOGICAL_DB}_publication FOR ALL TABLES;
+EOF
+    PGPASSWORD="$MASTER_PASSWORD" PGHOST=$MASTER_IP PGDATABASE=$LOGICAL_DB PGUSER=$MASTER_USER pg_dump -s $LOGICAL_DB > /root/$LOGICAL_DB.schema.sql
+    PGPASSWORD="$MASTER_PASSWORD" PGHOST=$(node_ip.sh) PGDATABASE=$LOGICAL_DB PGUSER=$MASTER_USER psql < /root/$LOGICAL_DB.schema.sql
+    PGPASSWORD="$MASTER_PASSWORD" PGHOST=$(node_ip.sh) PGDATABASE=$LOGICAL_DB PGUSER=$MASTER_USER psql <<EOF
+CREATE SUBSCRIPTION ${LOGICAL_DB}_${SERVER_ID}_subscription CONNECTION 'host=$MASTER_IP port=5432 password=$MASTER_PASSWORD user=$MASTER_USER dbname=$LOGICAL_DB' PUBLICATION ${LOGICAL_DB}_publication;
+EOF
+    touch /root/replication.configured
 fi
+
