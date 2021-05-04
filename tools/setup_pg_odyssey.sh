a94f7f9034bd7dfb0f9ce9d6cdb5443797befaef
Nickolay Ihalainen <ihanick@gmail.com>
2021-05-04 15:25:59 +0300

    FIX: Yandex Odyssey installation and setup

--- tools/setup_pg_odyssey.sh
+++ tools/setup_pg_odyssey.sh
@@ -7,7 +7,7 @@ PASS=$2
 PASS_HASH=$(echo -n "md5"; echo -n "$PASS$USER" | md5sum | awk '{print $1}')
 
 mkdir /etc/odyssey
-curl -sL --output /etc/odyssey/odyssey.conf https://github.com/yandex/odyssey/raw/master/odyssey.conf
+curl -sL --output /etc/odyssey/odyssey.conf https://github.com/yandex/odyssey/raw/1.1/odyssey.conf
 curl -SL --output /etc/systemd/system/odyssey.service https://raw.githubusercontent.com/yandex/odyssey/master/scripts/systemd/odyssey.service
 sed -i \
   -e 's/host "localhost"/host "'$DB_IP'"/' \
