6d367b5b705e557715d353e16e3009c5a659fc53
Nickolay Ihalainen <ihanick@gmail.com>
2021-05-14 10:13:20 +0300

    FIX: fix_wsrep_cluster_address.sh and install-only, FIX: MariaDB on el7 install compat package, IMP: MariaDB Cluster 10.6, FIX: galera package rhel7.el7 to el7 transition fixes issue #5

--- tools/fix_wsrep_cluster_address.sh
+++ tools/fix_wsrep_cluster_address.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 CNF_FILE=$1
-NODES=$(mysql -Ne "show status like 'wsrep_incoming_addresses'"|awk '{gsub(/:3306/,"");print $2}')
 
-[ "x$CNF_FILE" = "x" ] && CNF_FILE=$(cat /root/mysql.conf.filename)
+[ "x$CNF_FILE" = "x" -a -f /root/mysql.conf.filename ] && CNF_FILE=$(cat /root/mysql.conf.filename)
+
+if ! [ -f "$CNF_FILE" ] ; then exit 0 ; fi
+
+NODES=$(mysql -Ne "show status like 'wsrep_incoming_addresses'"|awk '{gsub(/:3306/,"");print $2}')
 
 if grep -q wsrep_cluster_address $CNF_FILE ; then
   sed -i -re "s|^\s*wsrep_cluster_address\s*=.*$|wsrep_cluster_address=gcomm://$NODES|" "$CNF_FILE"
