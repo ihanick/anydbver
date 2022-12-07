a52084e16d590404975048c418c5fbdc72621395
Nickolay Ihalainen <ihanick@gmail.com>
2022-12-07 18:34:25 +0300

    FIX: avoid using .. for tar path

--- ansible-ssh-docker/build.sh
+++ ansible-ssh-docker/build.sh
@@ -3,6 +3,8 @@ cd rockylinux8
 test -f ../../secret/id_rsa || ssh-keygen -t rsa -f ../../secret/id_rsa -P '' && chmod 0600 ../../secret/id_rsa
 cp ../../tools/node_ip.sh ../../secret/id_rsa.pub ./
 docker build -t rockylinux:8-sshd-systemd .
-cd ../ansible-anydbver
-tar -czf anydbver.tar.gz ../../
+cd ../..
+pwd
+tar -czf ansible-ssh-docker/ansible-anydbver/anydbver.tar.gz .
+cd ansible-ssh-docker/ansible-anydbver/
 docker build -t rockylinux:8-anydbver-ansible .
