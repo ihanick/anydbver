b03eee134401cd59645b927333a7077042572f04
Nickolay Ihalainen <ihanick@gmail.com>
2023-01-11 22:25:00 +0300

    REF: rename anydbver.py to anydbver, IMP: PSMDB 6.0, added version sqlite db

--- tools/setup_mongo_replication.sh
+++ tools/setup_mongo_replication.sh
@@ -5,16 +5,20 @@ PASS=$3
 PRIMARY=$4
 SERVER_ID=$(ip addr ls|grep 'inet '|grep -v '127.0.0.1'|awk '{print $2}'|cut -d/ -f 1|awk -F '\\.' '{print ($1 * 2^24) + ($2 * 2^16) + ($3 * 2^8) + $4}')
 
+MONGO=/usr/bin/mongo;
+test -f $MONGO || MONGO=/usr/bin/mongosh;
+
 if [[ "x$PRIMARY" == "x" ]] ; then
-  until mongo --eval 'print("waited for connection")' &>/dev/null ; do sleep 2 ; done
+  until $MONGO --eval 'print("waited for connection")' &>/dev/null ; do sleep 2 ; done
   if grep -q 'clusterRole: configsvr' /etc/mongod.conf ; then
-    mongo "mongodb://$USR:$PASS@127.0.0.1:27017/admin" --eval 'rs.initiate( { _id : "'$REPLICATION_SET'", configsvr: true, members: [ { _id: 0, host: "'$( hostname -I | cut -d' ' -f1 )':27017" }, ] })'
+    $MONGO "mongodb://$USR:$PASS@127.0.0.1:27017/admin" --eval 'rs.initiate( { _id : "'$REPLICATION_SET'", configsvr: true, members: [ { _id: 0, host: "'$( hostname -I | cut -d' ' -f1 )':27017" }, ] })'
   else
-    mongo "mongodb://$USR:$PASS@127.0.0.1:27017/admin" --eval 'rs.initiate( { _id : "'$REPLICATION_SET'", members: [ { _id: 0, host: "'$( hostname -I | cut -d' ' -f1 )':27017" }, ] })'
+    $MONGO "mongodb://$USR:$PASS@127.0.0.1:27017/admin" --eval 'rs.initiate( { _id : "'$REPLICATION_SET'", members: [ { _id: 0, host: "'$( hostname -I | cut -d' ' -f1 )':27017" }, ] })'
   fi
 else
   # wait until local mongod become ready
-  until mongo --eval 'print("waited for connection")' &>/dev/null ; do sleep 2 ; done
-  mongo "mongodb://$USR:$PASS@$PRIMARY:27017/admin" --eval 'rs.add({ host:"'$( hostname -I | cut -d' ' -f1 )':27017"})'
+  until $MONGO --eval 'print("waited for connection")' &>/dev/null ; do sleep 2 ; done
+  until $MONGO "mongodb://$USR:$PASS@$PRIMARY:27017/admin" --eval 'rs.status()' | grep -q PRIMARY ; do sleep 2 ; done
+  $MONGO "mongodb://$USR:$PASS@$PRIMARY:27017/admin" --eval 'rs.add({ host:"'$( hostname -I | cut -d' ' -f1 )':27017"})'
 fi
 touch /root/${REPLICATION_SET}.init
