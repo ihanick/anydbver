a88e03cd39734eefe2dbebb250f1e81f9f7b46fa
Nickolay Ihalainen <ihanick@gmail.com>
2023-06-28 16:13:26 +0300

    DOC: deploy --hepl for ldap and ldap-master

--- anydbver_help.py
+++ anydbver_help.py
@@ -6,6 +6,7 @@ def arg_help(name):
       "pmm-client": "pmm-client:ver,server=URL_OR_NODE",
       "k3d": "k3d:ver,cluster-domain=K8S_CLUSTER_DOMAIN,ingress=PORT,ingress-type=[nginx|traefik|traefik-metallb|istio",
       "percona-server-mongodb": "psmdb:version shardsrv configsrv replica-set:FIRST_SERVER mongos-cfg:RS/NODE,NODE,NODE mongos-shard:RS0/NODE,NODE,RS1/NODE,NODE...",
+      "ldap": "ldap, ldap-master:NODE",
       }
   examples = {
       "percona-server": "anydbver deploy ps:5.7.35 node1 ps:5.7.35,master=node0",
@@ -14,6 +15,7 @@ def arg_help(name):
       "pmm-client": "anydbver deploy node0 pmm:latest,docker-image,port=0.0.0.0:10443 node1 ps:5.7 pmm-client:2.37.1-6,server=node0",
       "k3d": "anydbver deploy k3d:latest,cluster-domain=percona.local,ingress=443,ingress-type=nginx,nodes=3 cert-manager k8s-mongo:1.14.0,cluster-name=db1",
       "percona-server-mongodb": "anydbver deploy psmdb replica-set:rs0 shardsrv  node1 psmdb master:default replica-set:rs0 shardsrv  node2 psmdb master:default replica-set:rs0 shardsrv node3 psmdb replica-set:rs1 shardsrv node4 psmdb replica-set:rs1 shardsrv master:node3 node5 psmdb replica-set:rs1 shardsrv master:node3 node6 psmdb configsrv replica-set:cfg0  node7 psmdb configsrv replica-set:cfg0 master:node6  node8 psmdb configsrv replica-set:cfg0 master:node6  node9 psmdb mongos-cfg:cfg0/node6,node7,node8 mongos-shard:rs0/default,node1,node2,rs1/node3,node4,node5",
+      "ldap": "anydbver deploy ldap node1 ldap-master:default psmdb:5.0",
       }
   if name in all_subargs and name in examples:
     return "R|{}\nEx. {}".format(all_subargs[name], examples[name])
