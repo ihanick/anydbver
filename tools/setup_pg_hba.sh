b9e80eb9cb3d8487b5eb0ee4aa8f9db1326d914f
Nickolay Ihalainen <ihanick@gmail.com>
2024-10-15 13:05:00 +0300

    FIX: pg docker-image restarts

--- tools/setup_pg_hba.sh
+++ tools/setup_pg_hba.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-if ! grep -q "host replication postgres" /docker-entrypoint-initdb.d/00-init.sh ; then
-	if test -z "$PGDATA" ; then
-		export PGDATA=/data/db
-	fi
-	cat >> /docker-entrypoint-initdb.d/00-init.sh <<EOF
-echo "host replication postgres all scram-sha-256" >> "$PGDATA"/pg_hba.conf
-EOF
-chmod +x /docker-entrypoint-initdb.d/00-init.sh
+if test -z "$PGDATA"; then
+  export PGDATA=/data/db
 fi
 
-if test -f /entrypoint.sh ; then 
+if ! grep -q "host replication postgres" /docker-entrypoint-initdb.d/00-init.sh 2>/dev/null; then
+  cat >>/docker-entrypoint-initdb.d/00-init.sh <<EOF
+echo "host replication postgres all scram-sha-256" >> "$PGDATA"/pg_hba.conf
+EOF
+  chmod +x /docker-entrypoint-initdb.d/00-init.sh
+fi
+
+if test -f /entrypoint.sh; then
   /entrypoint.sh postgres
 else
   docker-entrypoint.sh postgres
