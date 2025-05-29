3536c395e0fde11e4430bca63d7763c11b1031cb
Nickolay Ihalainen <nickolay.ihalainen@mydb.ru>
2025-05-29 09:45:01 +0300

    VER: update versions, version bump to 0.1.21

--- images-build/build.sh
+++ images-build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PLATFORM=linux/amd64
 IMAGE_PUBLISHER=ihanick
-IMAGE_VERSION="0.1.20"
+IMAGE_VERSION="0.1.21"
 PLATFORM_TAG=""
 if uname -m | egrep -q 'aarch64|arm64'; then
   PLATFORM=linux/arm64
